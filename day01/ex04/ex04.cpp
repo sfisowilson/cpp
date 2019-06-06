26f6d2cb4b3df55264e700d01f83024b988b95dc
sfisowilson <loverboy.sfiso@gmail.com>
2019-06-06 06:35:58 +0200

    five

--- day01/ex04/ex04.cpp
+++ day01/ex04/ex04.cpp
@@ -4,5 +4,10 @@ main (){
     std::string phrase = "HI THIS IS BRAIN";
     std::string *ptr = &phrase;
     std::string &ref = phrase;
+
+    std::cout << "string : "<<phrase<<std::endl;
+	std::cout << "pointer : " << *ptr << std::endl;
+	std::cout << "reference : " << ref << std::endl;
+    
     return 0;
 }
